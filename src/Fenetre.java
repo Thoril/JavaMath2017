c92baf2f61a5981707231eee2cf3ad9769f4d47c
Unknown <misterplaymania@gmail.com>
2017-12-20 15:44:05 +0100

    Fenêtre, début du menu + ouvertur fichier
    Debut d'une fenêtre graphique + ouverture de fichier csv possible. Récupération du chemin à utiliser par la suite.

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
 import javax.swing.border.Border;
-import java.awt.BorderLayout;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.FileFilter;
 
-public class Fenetre extends JFrame{
+public class Fenetre extends JFrame implements ActionListener{
 
     private JButton bouton1 = new JButton("Calculer");
     private JButton bouton2 = new JButton("Effacer");
 
     private JMenuBar menuBar = new JMenuBar();
-    private JMenu test1 = new JMenu("Fichier");
+    private JMenu fichier = new JMenu("Fichier");
     private JMenu test2 = new JMenu("Edition");
 
     private JMenuItem item1 = new JMenuItem("Ouvrir");
@@ -23,19 +29,38 @@ public class Fenetre extends JFrame{
         this.setLayout(new BorderLayout());
 
 
-
+        //Bouton en borderlayout sud
         JPanel sud = new JPanel();
         sud.add(bouton1);
         sud.add(bouton2);
 
         this.add(sud, BorderLayout.SOUTH);
 
-        this.test1.add(item1);
-        this.menuBar.add(test1);
+        item1.addActionListener(this);
+
+        //Menu
+        this.fichier.add(item1);
+        this.menuBar.add(fichier);
         this.setJMenuBar(menuBar);
+
 
         this.setVisible(true);
 
     }
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == item1){
+            System.out.println("test");
+            JFileChooser choix = new JFileChooser();
+            FileNameExtensionFilter MonFiltre = new FileNameExtensionFilter("Tableau", "csv");
+            choix.addChoosableFileFilter(MonFiltre);
+            choix.setAcceptAllFileFilterUsed(false);
+            int retour= choix.showOpenDialog(this);
+            if(retour==JFileChooser.APPROVE_OPTION){
+                String chemin =choix.getSelectedFile().getAbsolutePath();
+                System.out.print("Chemin: ");
+                System.out.println(chemin);
+            }
+        }
+    }
 }
